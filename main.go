f5448c2ccc60d92beaf80291fb2091645ae9f523
Alex Archer <archeralex@gmail.com>
2021-10-27 17:32:07 +0100

    Attempt at handling csv of any length

--- main.go
+++ main.go
@@ -19,16 +19,20 @@ func main() {
 	file, _ := os.Open(filename)
 	reader := csv.NewReader(file)
 
-	var user_answers [12]bool
-	for i := 0; i < 12; i++ {
-		line, _ := reader.Read()
-		question := line[0]
-		correct_answer := line[1]
-		fmt.Printf("What is the answer to: %v? ", question)
-		var user_answer string
-		fmt.Scanln(&user_answer)
-		fmt.Println()
-		user_answers[i] = user_answer == correct_answer
+	var user_answers []bool
+	for i, keep_reading := 0, true; keep_reading; i++ {
+		line, err := reader.Read()
+		if err != nil {
+			keep_reading = false
+		} else {
+			question := line[0]
+			correct_answer := line[1]
+			fmt.Printf("What is the answer to: %v? ", question)
+			var user_answer string
+			fmt.Scanln(&user_answer)
+			fmt.Println()
+			user_answers = append(user_answers, user_answer == correct_answer)
+		}
 	}
 	fmt.Println(user_answers)
 }
