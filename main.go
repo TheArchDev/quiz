576b34859f4b2dc05f0dd720839af1a57999c5c5
Alex Archer <archeralex@gmail.com>
2021-10-27 14:22:36 +0100

    Initial version: compares user answers to correct answers for specific case of 12 questions/answers

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println("Thanks for starting a quiz!")
+
+	file, _ := os.Open("problems.csv")
+	reader := csv.NewReader(file)
+
+	var user_answers [12]bool
+	for i := 0; i < 12; i++ {
+		line, _ := reader.Read()
+		question := line[0]
+		correct_answer := line[1]
+		fmt.Printf("What is the answer to: %v? ", question)
+		var user_answer string
+		fmt.Scanln(&user_answer)
+		fmt.Println()
+		user_answers[i] = user_answer == correct_answer
+	}
+	fmt.Println(user_answers)
 }
