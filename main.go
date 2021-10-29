846b50207a97927ed899915e7dfdc9105b6b14f6
Alex Archer <archeralex@gmail.com>
2021-10-29 12:41:27 +0100

    Refactor quiz method to not handle the reading of csv itself

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 var filename string
@@ -15,34 +16,45 @@ func init() {
 	flag.IntVar(&timer, "timer", 30, "Timer length in seconds")
 }
 
+func countdown(timer int) {
+	time.Sleep(time.Duration(timer) * time.Second)
+	fmt.Println("Timer over")
+}
+
+func run_quiz(data [][]string) int {
+	correct_answers := 0
+	for i := 0; i < len(data); i++ {
+		question := data[i][0]
+		correct_answer := data[i][1]
+
+		fmt.Printf("What is the answer to: %v? \n", question)
+		var user_answer string
+		fmt.Scanln(&user_answer)
+		fmt.Println()
+
+		if user_answer == correct_answer {
+			correct_answers += 1
+		}
+	}
+	return correct_answers
+}
 
 func main() {
 	flag.Parse()
+
 	fmt.Printf("Starting a quiz from %v!\n", filename)
-	fmt.Printf("Hit Enter to start the %v second(s) timer!\n", timer)
+	fmt.Printf("Hit Enter to start your %v second(s) timer!\n", timer)
 	fmt.Scanln()
+
+	go countdown(timer)
 
 	file, _ := os.Open(filename)
 	reader := csv.NewReader(file)
+	data, _ := reader.ReadAll()
+	number_of_questions := len(data)
 
-	correct_answers := 0
-	question_number := 0
-	for keep_reading := true; keep_reading; {
-		line, err := reader.Read()
-		if err != nil {
-			keep_reading = false
-		} else {
-			question := line[0]
-			correct_answer := line[1]
-			fmt.Printf("What is the answer to: %v? ", question)
-			var user_answer string
-			fmt.Scanln(&user_answer)
-			fmt.Println()
-			if user_answer == correct_answer {
-				correct_answers += 1
-			}
-			question_number++
-		}
-	}
-	fmt.Printf("Correctly answered %v out of %v question(s)\n", correct_answers, question_number)
+	correct_answers := run_quiz(data)
+
+	fmt.Printf("Correctly answered %v out of %v question(s)\n", correct_answers, number_of_questions)
+
 }
